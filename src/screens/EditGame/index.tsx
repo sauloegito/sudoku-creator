635c880d0f6ad0b01497ce0a55bf94ff7ba7baf8
Saulo Esteves Tabosa do Egito Santos <saulo.santos@serpro.gov.br>
2021-12-01 17:34:53 -0300

    Rename methods and imports

--- src/screens/EditGame/index.tsx
+++ src/screens/EditGame/index.tsx
@@ -1,24 +1,24 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
 import { TouchableOpacity, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/core";
 import { ControlNumber } from "../../components/ControlNumber";
 import { Sudoku } from "../../components/Sudoku";
-import { useGame } from "../../hooks/game";
-import { styles } from "./styles";
-import AppLoading from "expo-app-loading";
-import { GameValue } from "../../utils/types";
 import { NumberProps } from "../../components/NumberInput";
+import { useGame } from "../../hooks/game";
+import { GameValue } from "../../utils/types";
+import { styles } from "./styles";
 
 export function EditGame() {
   const { inGame, saveGameEdition, handleCellEditClick, isSelected } = useGame();
   const navigation = useNavigation();
 
-  function handleDiscartar() {
+  function handleDiscard() {
     navigation.goBack();
   }
 
-  function handlePersistir() {
+  function handleSave() {
     try {
       saveGameEdition();
     } finally {
@@ -49,10 +49,10 @@ export function EditGame() {
       <View style={styles.allControls}>
         <ControlNumber />
         <View style={styles.gameControls}>
-          <TouchableOpacity style={styles.item} onPress={handleDiscartar}>
+          <TouchableOpacity style={styles.item} onPress={handleDiscard}>
             <AntDesign name="closesquareo" size={24} style={styles.icon} />
           </TouchableOpacity>
-          <TouchableOpacity style={styles.item} onPress={handlePersistir}>
+          <TouchableOpacity style={styles.item} onPress={handleSave}>
             <AntDesign name="checksquareo" size={24} style={styles.icon} />
           </TouchableOpacity>
         </View>
